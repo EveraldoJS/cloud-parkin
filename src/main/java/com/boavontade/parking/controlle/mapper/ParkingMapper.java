74865d094fb6c7570860b0eef02a9868f7f77b70
EvrealdoJS <shiruba911@gmail.com>
2022-11-18 21:36:03 -0300

    Segundo endpoint

--- src/main/java/com/boavontade/parking/controlle/mapper/ParkingMapper.java
+++ src/main/java/com/boavontade/parking/controlle/mapper/ParkingMapper.java
@@ -1,5 +1,6 @@
 package com.boavontade.parking.controlle.mapper;
 
+import com.boavontade.parking.controlle.dto.ParkingCreateDTO;
 import com.boavontade.parking.controlle.dto.ParkingDTO;
 import com.boavontade.parking.model.Parking;
 import org.modelmapper.ModelMapper;
@@ -13,10 +14,18 @@ public class ParkingMapper {
 
     private static final ModelMapper MODEL_MAPPER = new ModelMapper();
 
-    public  ParkingDTO parkingDTO(Parking parking){
+    public  ParkingDTO toParkingDTO(Parking parking){
+
         return  MODEL_MAPPER.map(parking, ParkingDTO.class);
     }
     public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList){
-        return  parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+        return  parkingList.stream().map(this::toParkingDTO).collect(Collectors.toList());
+    }
+
+    public Parking toParking (ParkingDTO dto) {
+        return  MODEL_MAPPER.map(dto, Parking.class);
+    }
+    public Parking toParkingCreate(ParkingCreateDTO dto) {
+        return  MODEL_MAPPER.map(dto, Parking.class);
     }
 }
--- src/main/java/com/boavontade/parking/service/ParkingService.java
+++ src/main/java/com/boavontade/parking/service/ParkingService.java
@@ -2,6 +2,8 @@ package com.boavontade.parking.service;
 
 import com.boavontade.parking.model.Parking;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.HashMap;
@@ -10,18 +12,33 @@ import java.util.Map;
 @Service
 public class ParkingService {
 
-        private  static Map<String, Parking> parkingMap = new HashMap();
+    private static Map<String, Parking> parkingMap = new HashMap();
 
-    static  {
+    static {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id, "SSP-1234", "SP", "GOL", "BLUE");
+        Parking parking1 = new Parking(id1, "SPR-1234", "PR", "GM", "Pink");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
-    public List<Parking> findAll(){
+
+    public List<Parking> findAll() {
         return parkingMap.values().stream().collect(Collectors.toList());
     }
-private  static String getUUID(){
-        return UUID.randomUUID().toString().replace("-", "");
-}
 
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Parking findById(String id) {
+    return parkingMap.get(id);
+    }
+    public Parking create(Parking parkingCreate) {
+    String uuid = getUUID();
+    parkingCreate.setId(uuid);
+    parkingCreate.setEntryDate(LocalDateTime.now());
+    parkingMap.put(uuid, parkingCreate);
+    return  parkingCreate;
+    }
 }
